b769932920957527018136d23fe29e548b67b191
ij96 <ij96@users.noreply.github.com>
2018-01-10 18:27:49 +0000

    Modified .py (now outputs 2 dictionary files as .csv) - and renamed to "compare.py"

---
+++ compare.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3.5
+
+# combine two set of characters to generate two files:
+# - a complete dictionary, even if some char forms are the same
+# - a dictionary containing only char pairs with difference forms
+
+import sys
+
+delimiter = ','
+
+data_1 = []
+data_2 = []
+diff_char_1 = []
+diff_char_2 = []
+#diff_index = []
+
+with open(sys.argv[1], 'r') as file:
+	data_1 = file.read().splitlines()
+	file.close()
+
+with open(sys.argv[2], 'r') as file:
+	data_2 = file.read().splitlines()
+	file.close()
+
+if len(data_1) == len(data_2):
+	for i in range(len(data_1)):
+		if (data_1[i] != data_2[i]):
+			diff_char_1.append(data_1[i])
+			diff_char_2.append(data_2[i])
+			#diff_index.append(i)
+
+	with open(sys.argv[3], 'w') as out_file:
+		for i in range(len(data_1)):
+			out_file.write("%s%s%s\n" % (data_1[i],delimiter,data_2[i]))
+		out_file.close()
+
+	with open(sys.argv[4], 'w') as diff_file:
+		for i in range(len(diff_char_1)):
+			diff_file.write("%s%s%s\n" % (diff_char_1[i],delimiter,diff_char_2[i]))
+		diff_file.close()
+else:
+	print("Error: files do not have the same length")
